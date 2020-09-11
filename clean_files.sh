5a311797e2d42c7107de0c07624cc11400d58517
Luka Aghdgomelashvili <laghdgomelashvili@bog.ge>
2020-09-11 20:28:33 +0400

    all data

---
+++ clean_files.sh
@@ -0,0 +1,4 @@
+find bad_images -name '*.txt' ! -empty -type f -exec cp -vv {} labels/ \;
+find damchknari_images -name '*.txt' ! -empty -type f -exec cp -vv {} labels/ \;
+find secondary1_images -name '*.txt' ! -empty -type f -exec cp -vv {} labels/ \;
+find secondary2_images -name '*.txt' ! -empty -type f -exec cp -vv {} labels/ \;
